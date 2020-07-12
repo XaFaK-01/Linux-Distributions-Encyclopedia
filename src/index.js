225789d6f9691747e5029629f4c5b94b611e5344
XaFaK-01 <xafak01@gmail.com>
2020-07-13 04:40:28 +0500

    new features introduced

--- src/index.js
+++ src/index.js
@@ -1,67 +1,24 @@
 require("dotenv").config();
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const superagent = require("superagent");
-var _ = require("lodash");
-
-var string = require("lodash/string");
-
-const welcomeStartingContent =
-  "Find information regarding some of the latest and greatest GNU/Linux Distributions. Join the world of Free and Open Source Software by finding the perfect GNU/Linux Distribution best suites for your needs.";
-const aboutContent =
-  "GNU/Linux Distributions are various flavors of GNU/Linux Operating System. Each GNU/Linux Distribution have its own or inherited package manager, desktop environment, release type, backed community and various tools. Through this website you can find the perfect GNU/Linux distribution for your requirements.";
 const app = express();
+const appRoutes = require('./routes/appRoutes')
 var path = require("path");
-app.set("view engine", "ejs"); //by defining this line the res.render() will directly go into that folder to search for home
+
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
-app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static("public"));
+app.use(bodyParser.json({
+  limit: "50mb"
+}));
+app.use(bodyParser.urlencoded({
+  limit: "50mb",
+  extended: true,
+  parameterLimit: 50000
+}));
 app.use(express.static(path.join(__dirname, "./public")));
-
-const url = process.env.URL
-let port = process.env.PORT
-let distros = [];
-
-function updateDistrosList() {
-  superagent
-    .get(url)
-    .then((res) => {
-      distros = res.body;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
-updateDistrosList();
-
-app.get("/", async (req, res) => {
-  updateDistrosList();
-  res.render("home", { welcomeStartingContent, distros });
-});
-app.get("/about", (req, res) => {
-  res.render("about", { aboutContent });
-});
-
-app.get("/viewDistro/:distroName", (req, res) => {
-  updateDistrosList();
-  distros.forEach((distro) => {
-    if (req.params.distroName === distro.name) {
-      res.render("viewDistro", { distro });
-    }
-  });
-});
-
-app.post("/searchDistro", (req, res) => {
-  updateDistrosList();
-  const userInput = _.lowerCase(req.body.distroName);
-  distros.forEach((distro) => {
-    if (userInput === _.lowerCase(distro.name)) {
-      res.render("viewDistro", { distro });
-    }
-  });
-});
+app.use(appRoutes);
+let port = process.env.PORT;
 
 app.listen(port, function () {
   console.log("Server started on port 3000");
---
+++ src/routes/appRoutes.js
@@ -0,0 +1,183 @@
+var multer = require('multer')
+const sharp = require("sharp");
+const https = require('https')
+const superagent = require("superagent");
+const _ = require("lodash");
+const string = require("lodash/string");
+const express = require("express");
+const router = new express.Router();
+
+
+const welcomeStartingContent =
+    "Find information regarding some of the latest and greatest GNU/Linux Distributions. Join the world of Free and Open Source Software by finding the perfect GNU/Linux Distribution best suites for your needs.";
+const defineNewDistroContent =
+    "You can now define a GNU/Linux distribution here that's not defined yet by filling out the correct information in the below form. Keep in mind that this information will be first verified by admin and then posted.";
+
+const aboutContent =
+    "GNU/Linux Distributions are various flavors of GNU/Linux Operating System. Each GNU/Linux Distribution have its own or inherited package manager, desktop environment, release type, backed community and various tools. Through this website you can find the perfect GNU/Linux distribution for your requirements.";
+
+const url = 'http://lde-main.herokuapp.com/distros';
+let distros = [];
+
+function updateDistrosList() {
+    superagent
+        .get(url)
+        .then((res) => {
+            distros = res.body;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+updateDistrosList();
+
+router.get("/", async (req, res) => {
+    updateDistrosList();
+    res.render("home", {
+        welcomeStartingContent,
+        distros
+    });
+});
+router.get("/about", (req, res) => {
+    res.render("about", {
+        aboutContent
+    });
+});
+
+router.get("/viewDistro/:distroName", (req, res) => {
+    updateDistrosList();
+    distros.forEach((distro) => {
+        if (req.params.distroName === distro.name) {
+            res.render("viewDistro", {
+                distro
+            });
+        }
+    });
+});
+
+router.post("/searchDistro", (req, res) => {
+    updateDistrosList();
+    const userInput = _.lowerCase(req.body.distroName);
+    distros.forEach((distro) => {
+        if (userInput === _.lowerCase(distro.name)) {
+            res.render("viewDistro", {
+                distro
+            });
+        } else {
+            res.render('404page', {
+                message: "Sorry could not found that distribution! Why don't you define it and let it be approved and posted?"
+            })
+        }
+    });
+});
+
+router.get("/defineDistro", (req, res) => {
+    res.render("defineDistro", {
+        defineNewDistroContent
+    });
+});
+
+const upload = multer({
+    limits: {
+        fileSize: 2000000,
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|png|jpeg|svg)$/)) {
+            return cb(new Error("Please upload an image"));
+        }
+        cb(undefined, true); //means go ahead and accept the given upload
+    },
+});
+
+router.post("/postData", upload.any('photos'), async (req, resp) => {
+
+    console.log('data send here')
+    // console.log(req.body)
+    // console.log(req.files)
+
+    let distroLogo = await sharp(req.files[0].buffer)
+        .resize(400)
+        .png()
+        .toBuffer();
+
+    req.body.logo = distroLogo
+
+    const roundedCorners = Buffer.from(
+        '<svg><rect x="0" y="0" width="200" height="200" rx="100" ry="100"/></svg>'
+    );
+
+
+    if (req.files[1] === undefined) {
+        req.files[1] = "images/default-user"
+        let userPicture = await sharp(req.files[1])
+            .resize(200, 200)
+            .composite([{
+                input: roundedCorners,
+                blend: 'dest-in'
+            }])
+            .png()
+            .toBuffer();
+        req.body.userProfilePicture = userPicture
+    } else {
+        let userPicture = await sharp(req.files[1].buffer)
+            .resize(200, 200)
+            .composite([{
+                input: roundedCorners,
+                blend: 'dest-in'
+            }])
+            .png()
+            .toBuffer();
+        req.body.userProfilePicture = userPicture
+    }
+
+
+    const data = JSON.stringify(req.body)
+
+    let link = process.env.URL_NEW
+    const options = {
+        hostname: link,
+        port: 443,
+        path: '/distros',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length
+        }
+    }
+
+    const request = https.request(options, (res) => {
+        console.log(`statusCode: ${res.statusCode}`)
+
+        if (res.statusCode === 200 || res.statusCode === 201) {
+            return resp.render('statusPage', {
+                message: '👍 Data submitted for approval!'
+            })
+        }
+        resp.render('statusPage', {
+            message: "👎 Error occurred while submitting data! Make sure you haven't used a previously used distribution name"
+        })
+        request.on('data', (d) => {
+            process.stdout.write(d)
+        })
+    })
+
+    request.on('error', (error) => {
+        console.error(error)
+    })
+
+    request.write(data)
+    request.end()
+
+
+}), (error, req, res, next) => {
+    res.status(400).send({
+        error: error.message
+    })
+}
+
+router.get('*', (req, res) => {
+    res.render('404page')
+})
+
+
+module.exports = router;
